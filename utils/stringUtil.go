3a023b1872d763beffa9d3651f72bb1068f1451a
lxr <lxr18655043150@163.com>
2024-07-19 16:06:24 +0800

    添加string工具包

---
+++ utils/stringUtil.go
@@ -0,0 +1,19 @@
+package utils
+
+import "strings"
+
+var buffer strings.Builder
+
+// func init() {
+// 	buffer = strings.Builder{}
+// }
+
+func Concat(per, next, pattern string) string {
+	buffer.Reset()
+	buffer.WriteString(per)
+	if pattern != "" {
+		buffer.WriteString(pattern)
+	}
+	buffer.WriteString(next)
+	return buffer.String()
+}
